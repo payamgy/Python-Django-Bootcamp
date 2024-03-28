53fe30286ce5e57798e462910821e6a41f41f496
Sina Maleki <sina67631@gmail.com>
2024-03-28 09:41:02 +0330

    solved Q number q

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 # 08- March-28th Morning
 
-# Question  number 1
+# Question  number 6
+favorite_movies = [
+    ("Pulp fiction", 1994),
+    ("Inception", 2010),
+    ("x", 2020),
+    ("The Shawshank redemption", 1994)
+]
 
+new_list = sorted(favorite_movies, key=lambda x: x[1], reverse=True)
+
+counter = 0
+for item in new_list:
+    counter += 1
+    print(f"{counter}. {item}")
